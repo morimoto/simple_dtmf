b46ec1db5ff0b8abfaf1648f4ebf08dea87280ce
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2023-09-22 14:06:31 +0900

    Handle noise cancelling and multi DTMF
    It might analyze as "unknown" if data has big noise,
    and it can't analyze if data has multi DTMF data.
    
    This patch analyze the data via small pieces. It can cancel noise,
    and can analyze multi DTMF data by this patch.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "param.h"
 
 #define VERSION		"1.0.0"
+#define MAX_CHAN	16
 
 //=======================================
 //
@@ -25,8 +26,10 @@ static void usage(void)
 		"(input) simple_dtmf [v] -i file.wav\n\n"
 		"	-i : input file\n"
 		"	-v : verbose print\n\n"
-		"(info)  simple_dtmf -l file.wav\n",
-		VERSION
+		"(info)  simple_dtmf -l file.wav\n\n"
+		"note:\n"
+		"	max %d channels\n",
+		VERSION, MAX_CHAN
 		);
 }
 
@@ -99,6 +102,8 @@ static int parse_options(int argc, char **argv, struct dev_param *param)
 		goto err;
 	}
 	if (param->chan % 2)
+		goto err;
+	if (param->chan > MAX_CHAN)
 		goto err;
 
 	switch (param->rate) {
@@ -257,9 +262,17 @@ err:
 // dtmf_wav_analyze
 //
 //=======================================
+#define DEGREE	10	// 10%
 static int dtmf_wav_analyze(struct dev_param *param)
 {
 	int ret;
+	int challenge;
+	char *result;
+	char prev[MAX_CHAN];
+	int i, j;
+	int comma = 0;
+	int late_j = 0;
+	int width;
 
 	//==========================
 	// read wav header, and fill params
@@ -276,25 +289,188 @@ static int dtmf_wav_analyze(struct dev_param *param)
 	// alloc buf
 	ret = buf_alloc(param);
 	if (ret < 0)
-		goto err;
+		goto err_buff;
 
 	//==========================
 	// analyze DTMF
+	//
+	// Basic way is below, but it can't handle if data has too big noise,
+	// and/or if data has multi dtmfs.
+	// To reduse noise effect, and/or handle multi dtmfs, it will try to analyze by small pieces
+	//
+	// for (int i = 0; i < param->chan; i++) {
+	//	ret = wav_read_data(param, i);
+	//	...
+	//
+	//	printf("%c", dtmf_analyze(param->buf,
+	//				  param->length,
+	//				  param->rate));
+	// }
 	//==========================
-	for (int i = 0; i < param->chan; i++) {
+
+	// alloc for result
+	//
+	// wav_read_data() will read 1ch data each, and dtmf_analyze() will analyze specified data.
+	// We want to analyze 1ch data in 10% rate (= DEGREE) increments (= challenge),
+	// because it might include noise. Thus we need to keep the results.
+	//
+	// ex)
+	//	       <------ length ----- ... -->
+	//	       <- rate -><- rate -> ...
+	//	       <><><><><><><><><><>  (challenge)
+	//	buf = [xxxxxxxxxxxxxxxxxxxx ... xx]
+	//
+	// 1 rate   has DEGREE% challenges (= param->rate / 100 * DEGREE)
+	// Total challenges = param->length / (param->rate / 100 * DEGREE)
+	//		    = param->length * 100 / param->rate / DEGREE
+	challenge = param->length * 100 / param->rate / DEGREE;
+
+	// 1ch needs "challenge" results, and we has param->chan.
+	//
+	// "result" will keep each channels result
+	//
+	//	     <-- 1ch --><-- 2ch -->...
+	// result = [xxxxxxxxxxxyyyyyyyyyyy...]
+	//
+	result = calloc(param->chan, challenge);
+	if (!result)
+		goto err_buff;
+
+	// width = 1 challenge size
+	width = param->rate / 100 * DEGREE;
+
+	// analyze for each channels.
+	for (i = 0; i < param->chan; i++) {
+
+		// read each 1ch
 		ret = wav_read_data(param, i);
 		if (ret < 0)
 			goto free;
 
-		printf("%c", dtmf_analyze(param->buf,
-					  param->length,
-					  param->rate));
+		// analyze par 1 width
+		for (j = 0; j < challenge; j++)
+			result[challenge * i + j] = dtmf_analyze(param->buf + (width * j),
+								 width,
+								 param->rate);
 	}
+
+	if (param->flag & FLAG_VERBOSE)
+		for (i = 0; i < param->chan; i++) {
+			for (j = 0; j < challenge; j++)
+				printf("%c", result[challenge * i + j]);
+			printf("\n");
+		}
+
+	//
+	// In reality, data might have some dfmfs, and some parts might be noise
+	//
+	// <> : challenge
+	// vv : noise
+	//
+	//        vvvv               vvv          vvvv
+	// buf = [___xxxxxx____xxxxxxxx_____xxxxxxxx__]
+	//        <><><><><><><><><><><><><><><><><><>
+	//
+	// We keep prev "challenge" to judge either it was noise or not.
+	//
+	memset(&prev, 0, sizeof(prev));
+
+	//
+	// handle each challenge and print it
+	//
+	// ex)
+	//
+	// result = [?? 11 11 22 22 ?? 33 33 3? ?9 ?? ?4 44 44 ?? 55 55 55 ??]
+	//
+	// print = 11 22 33 ?9 44 55
+	//
+	for (j = 0; j < challenge; j++) {
+		int same = 0;
+		int uk = 0;
+
+		// check prev challenge, unknown
+		for (i = 0; i < param->chan; i++) {
+			if (result[challenge * i + j] == prev[i])
+				same++;
+			if (result[challenge * i + j] == unknown)
+				uk++;
+		}
+
+		//    skip if all data were same as prev data. (.. 33 33 ..)
+		// or skip if all data were unknown (noise)    (.. ?? ..)
+		if (same == param->chan ||
+		    uk   == param->chan)
+			continue;
+
+		// ex)
+		//
+		// result ~= [11 22 33 3? ?9 ?4 44 55]
+		//
+
+		// [.. 3? .. ?9 .. ?4 ..]
+		if (uk) {
+			//        *
+			// [.. 33 3? ..]
+			// it is noise
+			if (same > 0)
+				continue;
+
+			//      *
+			// [.. ?4 44 ..]
+			// it is also noise, but we can't judge it now.
+			// late judge
+			late_j = 1;
+			continue;
+		}
+
+		// We want like below
+		//	11,22,33,?9,44,55
+		if (comma)
+			printf(",");
+
+		if (late_j) {
+			int judge = 0;
+
+			//        *
+			// [.. ?4 44 ..]
+			// [.. ?9 ?4 ..]
+			for (i = 0; i < param->chan; i++)
+				if (result[challenge * i + j] != unknown &&
+				    result[challenge * i + j] == prev[i])
+					judge++;
+
+			//        *
+			// [.. ?4 44 ..]
+			// "?4" is noise
+
+			//        *
+			// [.. ?9 ?4 ..]
+			// "?9" is unknown data
+			if (judge)
+				for (i = 0; i < param->chan; i++)
+					printf("%c", prev[i]);
+		}
+
+		// print current data, and keep prev[]
+		for (i = 0; i < param->chan; i++) {
+			comma = 1;
+			late_j = 0;
+			printf("%c", result[challenge * i + j]);
+			prev[i] = result[challenge * i + j];
+		}
+	}
+
+	// all "?" case
+	if (!comma)
+		for (i = 0; i < param->chan; i++)
+			printf("%c", unknown);
 
 	// success
 	ret = 0;
 free:
 	printf("\n");
+	free(result);
+err_buff:
 	buf_free(param);
 err:
 	return ret;
